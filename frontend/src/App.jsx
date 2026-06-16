2002456ff61fc5386de568fce2dcae0e5e93a039
meena431 <meenakshithangadurai07@gmail.com>
2026-06-16 18:41:39 +0530

    Fix case-sensitivity bug in Home import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
-import Home from './pages/home';
+import Home from '../pages/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import ResultPage from './pages/ResultPage';
